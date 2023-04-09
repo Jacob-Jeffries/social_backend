c8b016e75fc30f865ae52d83194c587ac37eec8f
Jacob Jeffries <jeffrij85@gmail.com>
2023-04-09 16:06:39 -0400

    Create the Thoughts model

---
+++ server/models/Thought.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose')
+const reactionSchema = require('./Reactions')
+
+// Schema to create Thought Model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      require: true,
+      minLength: 1,
+      maxLength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    },
+    username: {
+      type: String,
+      require: true
+    },
+    reactions: [reactionSchema]
+  },
+  {
+    toJSON: {
+      virtuals: true
+    },
+    id: false
+  }
+)
+
+thoughtSchema
+  .virtual('reactionCount')
+  .get(function () {
+    return this.reactions.length
+  })
+  .virtual('createdAtFormatted')
+  // Getter
+  .get(function () {
+    return this.createdAt.toLocalString()
+  })
+
+const Thought = model('Thought', thoughtSchema)
+
+module.exports = Thought
